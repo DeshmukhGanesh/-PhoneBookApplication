fde6d1ce55b6ffbebd59f2f2d7ad2510b27bc33d
DeshmukhGanesh <deshmukhganeshb@gmail.com>
2022-08-25 12:00:47 +0530

    PBA-4:get all contacts from database

--- src/main/java/com/BikkadIT/controller/ContactController.java
+++ src/main/java/com/BikkadIT/controller/ContactController.java
@@ -1,8 +1,46 @@
 package com.BikkadIT.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.BikkadIT.model.Contact;
+import com.BikkadIT.service.ContactServiceI;
 
 @RestController
 public class ContactController {
 
+	@Autowired
+	private ContactServiceI contactServiceI;
+	
+	
+	@PostMapping(value = "/saveContact",consumes = "application/json")
+	public ResponseEntity<String> saveContact(@RequestBody Contact contact){
+		
+		boolean saveContact = contactServiceI.saveContact(contact);
+		if(saveContact==true) {
+			String msg="Contact Saved Successfully";
+			return new ResponseEntity<String>(msg,HttpStatus.OK);
+		}else {
+			String msg1="Contact not Saved";
+			return new ResponseEntity<String>(msg1,HttpStatus.BAD_REQUEST);	
+		}		
+	}
+	
+	@GetMapping(value = "/getAllContact",produces = "application/json")
+	public ResponseEntity<List<Contact>> getAllContact(){
+		List<Contact> allContacts = contactServiceI.getAllContacts();
+		if(allContacts != null) {
+			return new ResponseEntity<List<Contact>>(allContacts,HttpStatus.OK);
+		}else {
+			String msg="Data not found";
+			return new ResponseEntity(msg,HttpStatus.BAD_REQUEST);
+		}
+	}
 }
