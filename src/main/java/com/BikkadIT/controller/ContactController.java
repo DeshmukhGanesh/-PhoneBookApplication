afc31bfb03db453d03b424572245cb43107f1dbf
DeshmukhGanesh <deshmukhganeshb@gmail.com>
2022-08-25 12:43:17 +0530

    PBA-8:update operation

--- src/main/java/com/BikkadIT/controller/ContactController.java
+++ src/main/java/com/BikkadIT/controller/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,6 +50,18 @@ public class ContactController {
 	public ResponseEntity<Contact> getContactById(@PathVariable Integer cid){
 		Contact contactById = contactServiceI.getContactById(cid);
 		return new ResponseEntity<Contact>(contactById,HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/updateContact",consumes = "application/json")
+	public ResponseEntity<String> updateContact(@RequestBody Contact contact){
+		boolean updateContact = contactServiceI.updateContact(contact);
+		if(updateContact==true) {
+			String msg="Contact updated Successfully";
+			return new ResponseEntity<String>(msg,HttpStatus.OK);
+		}else {
+			String msg="Contact not updated Successfully";
+			return new ResponseEntity<String>(msg,HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	
